056715ce1a3db752249902d7b6fa5569f0d129db
Sarfaraz Haider Ali <sarfaraz6562@gmail.com>
2024-07-02 14:08:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // app.use(cors())
 app.use(
   cors({
-    origin: "https://yourfrontenddomain.com",
+    origin: "https://my-blog-backend-0s7a.onrender.com",
     methods: ["GET", "POST", "UPDATE", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
